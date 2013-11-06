36ebe043c74e457e35f747759d3d3efb8c4856a4
TongjieLi <snow8261@gmail.com>
2013-11-06 15:17:46 +0800

    pom

---
+++ gameoflife/src/main/java/com/snow8261/app/App.java
@@ -0,0 +1,13 @@
+package com.snow8261.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
